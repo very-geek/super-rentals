e44120dd3bbe50d10ef90711d36b8a2d487f2a8e
nightire <nightire@very-geek.com>
2020-02-10 20:22:08 +0800

    integrate with typescript support by ember-cli-typescript

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -1,6 +1,8 @@
-import Application from '../app';
-import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
+
+import Application from 'super-rentals/app';
+import config from 'super-rentals/config/environment';
+
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
